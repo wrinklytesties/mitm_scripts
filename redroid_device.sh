3f3ad0d01d6ea1cec4aa3dd9625d1380aea5c5d3
wrinklytesties <71526850+wrinklytesties@users.noreply.github.com>
2024-04-20 07:25:50 -0700

    fix: bad compatibility - this should work?

--- redroid_device.sh
+++ redroid_device.sh
@@ -13,7 +13,8 @@ log() {
 
 mkdir -p "$logdir"
 touch "$logfile"
-exec > >(tee -a "$logfile") 2>&1
+exec 1>>"$logfile" 2>>"$logfile"
+tail -f "$logfile" &
 
 if [ "$(id -u)" -ne 0 ]; then
     log '[shell] root required, re-run as root.'
